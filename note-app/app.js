ba16b852f5e3f27a1a8693b0c32e5e0823f43f7b
Olaide <olaidemmanuel1@gmail.com>
2020-01-09 13:40:05 +0100

    Started with my node course on #100dayscodechallenge

---
+++ note-app/app.js
@@ -0,0 +1,20 @@
+// const fs = require ('fs')
+
+// fs.appendFileSync('notes.txt', ' My name is Emmanuel Olaide')
+
+// fs.writeFileSync('notes.txt', 'This file was created by nodejs')
+
+// const validator = require('validator')
+const chalk = require ('chalk')
+const getNotes = require('./notes.js')
+
+const myBook = getNotes()
+console.log(myBook)
+
+// console.log(validator.isURL('https://olaide1191@gmail.com'))
+
+const color = chalk.blue.bold.inverse('This is my excercise')
+
+console.log((color))
+
+console.log(process.argv[2])
---
+++ note-app/notes.js
@@ -0,0 +1,4 @@
+const getNotes = function (){
+    return 'getNotes'
+}
+module.exports = getNotes
