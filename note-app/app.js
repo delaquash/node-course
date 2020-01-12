9b199518d4d0159c46d7fd13e7c01aaf4195bdee
Olaide <olaidemmanuel1@gmail.com>
2020-01-12 04:35:04 +0100

    Still working on yargs part

--- note-app/app.js
+++ note-app/app.js
@@ -5,21 +5,60 @@
 // fs.writeFileSync('notes.txt', 'This file was created by nodejs')
 
 // const validator = require('validator')
-const chalk = require ('chalk')
-const getNotes = require('./notes.js')
+const chalk = require ('chalk');
+const yargs = require('yargs');
+const getNotes = require('./notes.js');
 
-const myBook = getNotes()
-console.log(myBook)
+// console.log(process.argv)
+yargs.version('1.1.0');
+console.log(yargs.argv);
+
+// Create Add Command
+yargs.command({
+    command: 'add',
+    describe: 'add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption:!false,
+            type:'string'
+        }
+    },
+    handler:function(argv){
+        console.log('Title: ' + argv.title);
+    }
+});
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing a note');
+    }
+});
+
+// Create read
+yargs.command({
+    command: 'read',
+    describe:'I am currently reading on ALibaba Journey',
+    handler: function(){
+        console.log('Latest book i read');
+    }
+})
+
+// const myBook = getNotes()
+// console.log(myBook)
 
 // console.log(validator.isURL('https://olaide1191@gmail.com'))
 
-const color = chalk.blue.bold.inverse('This is my excercise')
+// const color = chalk.blue.bold.inverse('This is my excercise')
 
-console.log((color))
-console.log (process.argv)
-const command = process.argv[2]
-if (command === 'add'){
-    console.log('adding note')
-} else if (command === 'remove'){
-    console.log('remove note')
-}
+// console.log((color))
+// console.log (process.argv)
+// const command = process.argv[2]
+// if (command === 'add'){
+//     console.log('adding note')
+// } else if (command === 'remove'){
+//     console.log('remove note')
+// }
