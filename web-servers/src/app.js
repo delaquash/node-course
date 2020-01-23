83890ed075959f9be2ac15d552919278ada5d8cc
Olaide <olaidemmanuel1@gmail.com>
2020-01-23 16:34:33 +0100

    Styling the page

--- web-servers/src/app.js
+++ web-servers/src/app.js
@@ -1,27 +1,38 @@
 const path = require('path');
 const express = require('express');
-
-const publicDirectoryPath = (path.join(__dirname, '../public'));
-
 const app = express();
+const hbs = require("hbs")
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../template/view')
+const partialsPath = path.join(__dirname, '../template/partials')
 
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
     res.render('index', {
+        name: 'Olaide Emmanuel',
         stack: 'React, Vue, NodeJS, Python, Golang, C##',
         profession: 'Software Developer',
         futureAspiration: 'IoT, ML'
     });
 });
 
-app.get('/about', (req, res)=> {
-    res.render('about',{
-        
+app.get('/about', (req, res) => {
+    res.render('about', {
+        stack: 'React, Vue, NodeJS, Python, Golang, C##',
+        profession: 'Software Developer',
+        futureAspiration: 'IoT, ML',
+        name: 'Olaide Emmanuel'
     });
-})
+});
 // app.com
 // app.com/help
 // app.com/about
@@ -32,9 +43,14 @@ app.get('', (req, res) => {
 })
 
 
-app.get('/about', (req, res) => {
-    res.send('This is our about page');
-})
+app.get('/help', (req, res) => {
+    res.render('help', {
+        stack: 'React and NodeJS',
+        message: "God help me",
+        new: ' Help',
+           name: 'Olaide Emmanuel'
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send({
@@ -43,10 +59,20 @@ app.get('/weather', (req, res) => {
     });
 });
 
+app.get('/help/*', (req, res) => {
+    res.send('Help articles not found');
+})
 
-
+app.get ("*", (req, res) => {
+      res.render('404', {
+        name: 'Olaide Emmanuel',
+        title: '404',
+        errorMessage:'Page not found',
+    });
+});
 
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.');
 });
+
