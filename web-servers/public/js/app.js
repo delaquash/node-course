e0c4f91c943fdff158d48e91ac18e728827e7a65
Olaide <olaidemmanuel1@gmail.com>
2020-01-28 15:21:11 +0100

    Styled the user interface

--- web-servers/public/js/app.js
+++ web-servers/public/js/app.js
@@ -11,17 +11,23 @@ console.log('Clients side JS is loading');
 
 const weatherForm = document.querySelector("form");
 const search = document.querySelector('input');
-weatherForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const location = search.value;
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
-        fetch('http://localhost:3000/weather?address=' + location).then((response) =>{
-        response.json().then((data) => {
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const location = search.value
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = '...'
+
+        fetch('http://localhost:3000/weather?address=' + location).then((res) =>{
+        res.json().then((data) => {
             if(data.error) {
-                console.log(data.error);
-            } else{
-                console.log(data.location);
-                console.log(data.forecast);
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
             }
         });
     });
