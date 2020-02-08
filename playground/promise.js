f666909272bb77dd42f669dfce675960a029b96c
Olaide <olaidemmanuel1@gmail.com>
2020-02-08 23:32:42 +0100

    Connected to a database using mongodb

--- playground/promise.js
+++ playground/promise.js
@@ -1,5 +1,12 @@
 const doWorkPromise = new Promise ((resolve, reject) => {
     setTimeout(() => {
-        resolve ([7, 3, 5])
+        // resolve ([7, 3, 5]);
+        reject ([11, 13, 17]);
     }, 2000)
+})
+
+doWorkPromise.then((result) => {
+    console.log('Success', result);
+}).catch((error) => {
+    console.log("Error", error);
 })
