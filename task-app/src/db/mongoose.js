ccdc8e7258955c99e2f78cc2180429716c93bdfa
Olaide <olaidemmanuel1@gmail.com>
2020-02-18 15:23:10 +0100

    Installled validator and mongoose packages

--- task-app/src/db/mongoose.js
+++ task-app/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-app-api", {
     useNewUrlParser: true,
@@ -10,41 +11,75 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-app-api", {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        length: 6,
+        validate(value) {
+            if(value.toLowercase().includes("password")) {
+                throw new Error("Password must not be more six characters")
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase:true,
+        // Using validator npm to validate an email whether it is real or not
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error("This must be a valid email")
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value < 0) {
+                throw new Error('Age must be a positive number');
+            }
+        }
     }
 });
 
 
 const me = new User ({
     name: 'Emmanuel',
-    age: 27
-})
+    age: 27,
+    // Throws an error becoause they email code below is invalid
+    email:'mike@'
+});
 
 me.save().then((me) => {
     console.log(me)
 }).catch((error)=> {
-    console.log('Error', error)
-})
+    console.log('Error', error);
+});
 
 
 // Example of how to create a mongoose model
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
-    completed : {
-        type: Boolean
+    completed: {
+        type: Boolean,
+        default: false
     }
 })
 
 
 const task = new Task ({
-    description: "Learn the Mongoose library",
-    completed: false
+    description: 'My goal is to become a competent software developer'
 })
 
 task.save().then(() => {
